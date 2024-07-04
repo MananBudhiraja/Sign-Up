618815d9def8ae0e9a5606e76ca55dd1d5e5be9f
MananBudhiraja <85609636+MananBudhiraja@users.noreply.github.com>
2024-07-04 14:12:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your frontend's origin
+    origin: ['https://sign-up-fw54.vercel.app/'], // Replace with your frontend's origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
