eb2f81ae54fd042b8167845c7694feae3eb0b857
MananBudhiraja <85609636+MananBudhiraja@users.noreply.github.com>
2024-07-04 14:38:30 +0530

    Update Signup.jsx

--- vite-project/src/Signup.jsx
+++ vite-project/src/Signup.jsx
@@ -25,7 +25,7 @@ function Signup() {
             return;
         }
 
-        axios.post('https://sign-up-56qk.vercel.app/register', { name, email, password })
+        axios.post('https://sign-up-vjcb.vercel.app//register', { name, email, password })
             .then(result => {
                 setSuccess("Registration successful!");
                 setError("");
